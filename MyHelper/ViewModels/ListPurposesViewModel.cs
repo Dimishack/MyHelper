1c6534a254156c2d98507fbed096310c2bb2204c
Dimishack <dimishak.git@mail.ru>
2024-02-23 19:01:29 +0300

    Добавление счетчиков количества выполненных целей и количества целей и ProgressBar

--- MyHelper/ViewModels/ListPurposesViewModel.cs
+++ MyHelper/ViewModels/ListPurposesViewModel.cs
@@ -9,127 +9,148 @@ using System.Windows.Input;
 
 namespace MyHelper.ViewModels
 {
-	class ListPurposesViewModel : ViewModel
-	{
-		public ObservableCollection<MyPurposes>? MyPurposes { get; }
+    class ListPurposesViewModel : ViewModel
+    {
+        public ObservableCollection<MyPurposes>? MyPurposes { get; }
 
-		#region SelectedListMyPurposes : MyPurposes - Выбранный список целей
+        #region SelectedListMyPurposes : MyPurposes - Выбранный список целей
 
-		///<summary>Выбранный список целей</summary>
-		private MyPurposes? _selectedListMyPurposes;
+        ///<summary>Выбранный список целей</summary>
+        private MyPurposes? _selectedListMyPurposes;
 
-		///<summary>Выбранный список целей</summary>
-		public MyPurposes? SelectedListMyPurposes { get => _selectedListMyPurposes; set => Set(ref _selectedListMyPurposes, value); }
+        ///<summary>Выбранный список целей</summary>
+        public MyPurposes? SelectedListMyPurposes
+        {
+            get => _selectedListMyPurposes;
+            set
+            {
+                if (Set(ref _selectedListMyPurposes, value))
+                {
+                    OnPropertyChanged(nameof(MyPurposesCount));
+                    OnPropertyChanged(nameof(CompletingMyPurposesCount));
+                    OnPropertyChanged(nameof(Percent));
+                }
+            }
+        }
+        #endregion
 
-		#endregion
+        #region SelectedMyPurpose : MyPurpose - Выбранная цель
 
-		#region SelectedMyPurpose : MyPurpose - Выбранная цель
+        ///<summary>Выбранная цель</summary>
+        private MyPurpose? _selectedMyPurpose;
 
-		///<summary>Выбранная цель</summary>
-		private MyPurpose? _selectedMyPurpose;
+        ///<summary>Выбранная цель</summary>
+        public MyPurpose? SelectedMyPurpose { get => _selectedMyPurpose; set => Set(ref _selectedMyPurpose, value); }
 
-		///<summary>Выбранная цель</summary>
-		public MyPurpose? SelectedMyPurpose { get => _selectedMyPurpose; set => Set(ref _selectedMyPurpose, value); }
+        #endregion
 
-		#endregion
+        public int MyPurposesCount => SelectedListMyPurposes is null
+            ? 0
+            : SelectedListMyPurposes.ListPurposes.Count;
 
-		#region Команды
-		
-		#region CreateNewPurposeCommand - Команда создания цели
+        public int CompletingMyPurposesCount => SelectedListMyPurposes is null
+            ? 0
+            : SelectedListMyPurposes.ListPurposes.Where(p => p.IsCompleted).Count();
 
-		///<summary>Команда создания цели</summary>
-		private ICommand? _сreateNewPurposeCommand;
+        public double Percent => (double)CompletingMyPurposesCount / MyPurposesCount;
 
-		///<summary>Команда создания цели</summary>
-		public ICommand CreateNewPurposeCommand => _сreateNewPurposeCommand
-			??= new LambdaCommand(OnCreateNewPurposeCommandExecuted, CanCreateNewPurposeCommandExecute);
+        #region Команды
 
-		///<summary>Проверка возможности выполнения - Команда создания цели</summary>
-		private bool CanCreateNewPurposeCommandExecute(object? p) => true;
+        #region CreateNewPurposeCommand - Команда создания цели
 
-		///<summary>Логика выполнения - Команда создания цели</summary>
-		private void OnCreateNewPurposeCommandExecuted(object? p)
-		{
-			((Command)SaveListMyPurposesCommand).Executable = true;
-		}
+        ///<summary>Команда создания цели</summary>
+        private ICommand? _сreateNewPurposeCommand;
 
-		#endregion
+        ///<summary>Команда создания цели</summary>
+        public ICommand CreateNewPurposeCommand => _сreateNewPurposeCommand
+            ??= new LambdaCommand(OnCreateNewPurposeCommandExecuted, CanCreateNewPurposeCommandExecute);
 
-		#region DeletePurposeCommand - Команда удаления цели
+        ///<summary>Проверка возможности выполнения - Команда создания цели</summary>
+        private bool CanCreateNewPurposeCommandExecute(object? p) => true;
 
-		///<summary>Команда удаления цели</summary>
-		private ICommand? _deletePurposeCommand;
-
-		///<summary>Команда удаления цели</summary>
-		public ICommand DeletePurposeCommand => _deletePurposeCommand
-			??= new LambdaCommand(OnDeletePurposeCommandExecuted, CanDeletePurposeCommandExecute);
-
-		///<summary>Проверка возможности выполнения - Команда удаления цели</summary>
-		private bool CanDeletePurposeCommandExecute(object? p) => SelectedMyPurpose is not null;
-
-		///<summary>Логика выполнения - Команда удаления цели</summary>
-		private void OnDeletePurposeCommandExecuted(object? p)
+        ///<summary>Логика выполнения - Команда создания цели</summary>
+        private void OnCreateNewPurposeCommandExecuted(object? p)
         {
             ((Command)SaveListMyPurposesCommand).Executable = true;
         }
 
-		#endregion
+        #endregion
 
-		#region EditMyPurposeCommand - Редактирование цели
+        #region DeletePurposeCommand - Команда удаления цели
 
-		///<summary>Редактирование цели</summary>
-		private ICommand? _editMyPurposeCommand;
+        ///<summary>Команда удаления цели</summary>
+        private ICommand? _deletePurposeCommand;
 
-		///<summary>Редактирование цели</summary>
-		public ICommand EditMyPurposeCommand => _editMyPurposeCommand
-			??= new LambdaCommand(OnEditMyPurposeCommandExecuted, CanEditMyPurposeCommandExecute);
+        ///<summary>Команда удаления цели</summary>
+        public ICommand DeletePurposeCommand => _deletePurposeCommand
+            ??= new LambdaCommand(OnDeletePurposeCommandExecuted, CanDeletePurposeCommandExecute);
 
-		///<summary>Проверка возможности выполнения - Редактирование цели</summary>
-		private bool CanEditMyPurposeCommandExecute(object? p) => SelectedMyPurpose is not null;
+        ///<summary>Проверка возможности выполнения - Команда удаления цели</summary>
+        private bool CanDeletePurposeCommandExecute(object? p) => SelectedMyPurpose is not null;
 
-		///<summary>Логика выполнения - Редактирование цели</summary>
-		private void OnEditMyPurposeCommandExecuted(object? p)
+        ///<summary>Логика выполнения - Команда удаления цели</summary>
+        private void OnDeletePurposeCommandExecuted(object? p)
         {
             ((Command)SaveListMyPurposesCommand).Executable = true;
         }
 
-		#endregion
+        #endregion
 
-		#region SaveListMyPurposesCommand - Команда сохранения списка целей
+        #region EditMyPurposeCommand - Редактирование цели
 
-		///<summary>Команда сохранения списка целей</summary>
-		private ICommand? _saveListMyPurposesCommand;
+        ///<summary>Редактирование цели</summary>
+        private ICommand? _editMyPurposeCommand;
 
-		///<summary>Команда сохранения списка целей</summary>
-		public ICommand SaveListMyPurposesCommand => _saveListMyPurposesCommand
-			??= new LambdaCommand(OnSaveListMyPurposesCommandExecuted, CanSaveListMyPurposesCommandExecute);
+        ///<summary>Редактирование цели</summary>
+        public ICommand EditMyPurposeCommand => _editMyPurposeCommand
+            ??= new LambdaCommand(OnEditMyPurposeCommandExecuted, CanEditMyPurposeCommandExecute);
 
-		///<summary>Проверка возможности выполнения - Команда сохранения списка целей</summary>
-		private bool CanSaveListMyPurposesCommandExecute(object? p) => true;
+        ///<summary>Проверка возможности выполнения - Редактирование цели</summary>
+        private bool CanEditMyPurposeCommandExecute(object? p) => SelectedMyPurpose is not null;
 
-		///<summary>Логика выполнения - Команда сохранения списка целей</summary>
-		private void OnSaveListMyPurposesCommandExecuted(object? p)
+        ///<summary>Логика выполнения - Редактирование цели</summary>
+        private void OnEditMyPurposeCommandExecuted(object? p)
+        {
+            ((Command)SaveListMyPurposesCommand).Executable = true;
+        }
+
+        #endregion
+
+        #region SaveListMyPurposesCommand - Команда сохранения списка целей
+
+        ///<summary>Команда сохранения списка целей</summary>
+        private ICommand? _saveListMyPurposesCommand;
+
+        ///<summary>Команда сохранения списка целей</summary>
+        public ICommand SaveListMyPurposesCommand => _saveListMyPurposesCommand
+            ??= new LambdaCommand(OnSaveListMyPurposesCommandExecuted, CanSaveListMyPurposesCommandExecute);
+
+        ///<summary>Проверка возможности выполнения - Команда сохранения списка целей</summary>
+        private bool CanSaveListMyPurposesCommandExecute(object? p) => true;
+
+        ///<summary>Логика выполнения - Команда сохранения списка целей</summary>
+        private void OnSaveListMyPurposesCommandExecuted(object? p)
         {
             ((Command)SaveListMyPurposesCommand).Executable = false;
         }
 
-		#endregion
+        #endregion
 
-		#endregion
+        #endregion
 
 
-		public ListPurposesViewModel()
-		{
-			((Command)SaveListMyPurposesCommand).Executable = false;
-			MyPurposes = new ObservableCollection<MyPurposes>(Enumerable.Range(0, 10).Select(p => new MyPurposes
-			{
-				Year = DateTime.Now.Year + p,
-				Name = $"Name {p}",
-				ListPurposes = new ObservableCollection<MyPurpose>(Enumerable.Range(1, 10000).Select(p => new MyPurpose
-				{
-					Purpose = p.ToString(),
-				}))
-			}));
-		}
-	}
+        public ListPurposesViewModel()
+        {
+            ((Command)SaveListMyPurposesCommand).Executable = false;
+            MyPurposes = new ObservableCollection<MyPurposes>(Enumerable.Range(0, 100).Select(p => new MyPurposes
+            {
+                Year = DateTime.Now.Year + p,
+                Name = $"Name {p}",
+                ListPurposes = new ObservableCollection<MyPurpose>(Enumerable.Range(1, 23).Select(p => new MyPurpose
+                {
+                    Purpose = p.ToString(),
+                }))
+            }));
+        }
+    }
 }
