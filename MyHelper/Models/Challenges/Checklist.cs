da5f2baf124748ed1199876309133900ed4f85ae
Dimishack <dimishak.git@mail.ru>
2024-05-08 15:49:03 +0300

    Классы MyChallenges, MyChallenge и CheckList

--- MyHelper/Models/Challenges/Checklist.cs
+++ MyHelper/Models/Challenges/Checklist.cs
@@ -1,16 +1,29 @@
-﻿using MyHelper.ViewModels.Base;
-using System;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace MyHelper.Models.Challenges
 {
-    internal class Checklist(string time, bool check) : ViewModel
+    internal class Checklist : INotifyPropertyChanged
     {
-        public string Time { get; set; } = time;
-        private bool _check = check;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int NumberDay { get; set; }
+        public string Date { get; set; } = string.Empty;
+        public string DayOfWeek { get; set; } = string.Empty;
+
+        private bool _check;
         public bool Check
         {
             get => _check; 
-            set => Set(ref _check, value);
+            set
+            {
+                if(Equals(_check, value)) return;
+                _check = value;
+                OnPropertyChanged();
+            }
         }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
     }
 }
---
+++ MyHelper/Models/Challenges/MyChallenge.cs
@@ -0,0 +1,29 @@
+﻿using MyHelper.ViewModels.Base;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MyHelper.Models.Challenges
+{
+    internal class MyChallenge : ViewModel
+    {
+        private bool _isProgress;
+        [JsonProperty("Выполняется?")]
+        public bool IsProgress
+        {
+            get => _isProgress;
+            set => Set(ref _isProgress, value);
+        }
+        private string? _challenge;
+        [JsonProperty("Челлендж")]
+        public string? Challenge { get => _challenge; set => Set(ref _challenge, value); }
+
+        [JsonProperty("Начало челленджа")]
+        public string? DateStartProgressing { get; set; }
+
+        [JsonProperty("Примечание")]
+        public string? Note { get; set; }
+
+        [JsonProperty("Чек-лист")]
+        public IList<Checklist>? Checklist { get; set; }
+    }
+}
