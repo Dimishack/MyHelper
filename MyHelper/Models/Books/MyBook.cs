5228816c3ee98721b03b5c36c0d6d16b535c4dae
Dimishack <dimishak.git@mail.ru>
2024-06-30 16:01:52 +0300

    Изменение модуля книг

---
+++ MyHelper/Models/Books/MyBook.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyHelper.Models.Books
+{
+    class MyBook : INotifyPropertyChanged
+    {
+        public string? Author { get; set; }
+        public string? Name { get; set; }
+        public ushort Pages { get; set; }
+
+        private bool _isReaded;
+        public bool IsReaded
+        {
+            get => _isReaded;
+            set
+            {
+                if (Equals(value, _isReaded)) return;
+                _isReaded = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isReading;
+        public bool IsReading
+        {
+            get => _isReading;
+            set
+            {
+                if(Equals(value, _isReading)) return;
+                _isReading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
--- MyHelper/Models/Books/MyBooks.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyHelper.Models.Books
-{
-    internal class MyBooks
-    {
-        public string? Author { get; set; }
-        public string? Title { get; set; }
-        public bool? IsRead { get; set; }
-    }
-}
