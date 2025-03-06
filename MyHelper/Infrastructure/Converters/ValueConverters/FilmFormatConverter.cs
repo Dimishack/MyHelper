670e3c97e20bea439f430dc6f193eaf03a418e5e
Dimishack <dimishak.git@mail.ru>
2025-03-06 15:07:13 +0300

    Получение списка форматов и статусов с перечислителей

--- MyHelper/Infrastructure/Converters/ValueConverters/FilmFormatConverter.cs
+++ MyHelper/Infrastructure/Converters/ValueConverters/FilmFormatConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using MyHelper.Models.Enums;
+using System.Globalization;
 using System.Windows.Data;
 
 namespace MyHelper.Infrastructure.Converters.ValueConverters
@@ -6,11 +7,16 @@ namespace MyHelper.Infrastructure.Converters.ValueConverters
     [ValueConversion(typeof(int), typeof(string))]
     internal class FilmFormatConverter : Base.ValueConverter
     {
-        private readonly string[] _formats = ["Полнометражный фильм", "Короткометражный фильм", "Сериал", "Мини-сериал", "Телевизионный фильм",
+        private readonly string[] _formats = Enum.GetNames(typeof(FilmFormat));
+        private readonly string[] _formats_ru = ["Полнометражный фильм", "Короткометражный фильм", "Сериал", "Мини-сериал", "Телевизионный фильм",
                                 "Веб-сериал", "Реалити-шоу", "Ток-шоу", "Концерт", "Музыкальное видео"];
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-            value is not int format ? string.Empty
-            : format >= _formats.Length || format < 0 ? "Неизвестный формат!"
-            : _formats[format];
+            value is not int format
+            ? string.Empty
+            : format >= _formats_ru.Length && format < _formats.Length
+            ? _formats[format]
+            : format >= _formats.Length || format < 0
+            ? "Неизвестный формат!"
+            : _formats_ru[format];
     }
 }
--- MyHelper/Infrastructure/Converters/ValueConverters/FilmStatusConverter.cs
+++ MyHelper/Infrastructure/Converters/ValueConverters/FilmStatusConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using MyHelper.Models.Enums;
+using System.Globalization;
 using System.Windows.Data;
 
 namespace MyHelper.Infrastructure.Converters.ValueConverters
@@ -6,10 +7,15 @@ namespace MyHelper.Infrastructure.Converters.ValueConverters
     [ValueConversion(typeof(int), typeof(string))]
     class FilmStatusConverter : Base.ValueConverter
     {
-        private readonly string[] _statuses = ["Запланирован", "Смотрю", "Посмотрен"];
+        private readonly string[] _statuses = Enum.GetNames(typeof(FilmStatus));
+        private readonly string[] _statuses_ru = ["Запланирован", "Смотрю", "Посмотрен"];
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => value is not int status ? string.Empty
-            : status >= _statuses.Length || status < 0 ? "Неизвестный статус!"
-            : _statuses[status];
+            => value is not int status
+            ? string.Empty
+            : status >= _statuses.Length && status < _statuses.Length
+            ? _statuses[status]
+            : status >= _statuses_ru.Length || status < 0
+            ? "Неизвестный статус!"
+            : _statuses_ru[status];
     }
 }
